d8f86b12659e82f2722342dade2165c8bed0d4c5
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2020-07-14 18:37:19 -0300

    Modificação do sistema vizualização de recados, modificando o user id para o nome do aluno

--- app/src/main/java/com/cristianerm/pd2/RecadosCustomAdapter.java
+++ app/src/main/java/com/cristianerm/pd2/RecadosCustomAdapter.java
@@ -1,6 +1,7 @@
 package com.cristianerm.pd2;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,8 +15,11 @@ import androidx.annotation.NonNull;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -26,6 +30,7 @@ public class RecadosCustomAdapter extends BaseAdapter implements ListAdapter {
     private FirebaseDatabase mFirebaseDatase;
     private FirebaseAuth mAuth;
     private DatabaseReference myRef;
+    private DatabaseReference myRef2;
     private String userID;
 
 
@@ -59,7 +64,7 @@ public class RecadosCustomAdapter extends BaseAdapter implements ListAdapter {
         FirebaseUser user = mAuth.getCurrentUser();
         userID = user.getUid();
         myRef = mFirebaseDatase.getReference().child("recados_lidos");
-
+        myRef2 = mFirebaseDatase.getReference().child(userID).child("info_user");
 
         if (view == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -71,14 +76,30 @@ public class RecadosCustomAdapter extends BaseAdapter implements ListAdapter {
         listItemText.setText(recadosList.get(position));
 
         //Handle buttons and add onClickListeners
-        Button buttonVisto = (Button)view.findViewById(R.id.buttonRecadoVisto);
+        final Button buttonVisto = (Button)view.findViewById(R.id.buttonRecadoVisto);
 
-        buttonVisto.setOnClickListener(new View.OnClickListener(){
+        myRef2.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                String key = myRef.push().getKey();
-                myRef.child(key).child("mensagem_lida").setValue(recadosList.get(position));
-                myRef.child(key).child("user").setValue(userID);
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    UserInformation uInfo = new UserInformation();
+                    uInfo.setNome(ds.getValue(UserInformation.class).getNome());
+
+                     final String nome_aluno = uInfo.getNome();
+                    buttonVisto.setOnClickListener(new View.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            String key = myRef.push().getKey();
+                            myRef.child(key).child("mensagem_lida").setValue(recadosList.get(position));
+                            myRef.child(key).child("user").setValue(nome_aluno);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
 
