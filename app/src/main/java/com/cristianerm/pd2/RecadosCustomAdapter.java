49dbace592772b8d6d269fa4e9eabe96f14b4e1e
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2020-09-21 18:24:51 -0300

    Gets recados that the current user already clicked and blocks button and blocks button when the user clicks

--- app/src/main/java/com/cristianerm/pd2/RecadosCustomAdapter.java
+++ app/src/main/java/com/cristianerm/pd2/RecadosCustomAdapter.java
@@ -110,8 +110,23 @@ public class RecadosCustomAdapter extends BaseAdapter implements ListAdapter {
                                 rInfo.setDate(ds.getValue(RecadosInformation.class).getDate());
                                 rInfo.setTextInfo(ds.getValue(RecadosInformation.class).getTextInfo());
 
-                                Log.d("RecadosCustomAdapter", "date: " + rInfo.getDate());
-                                Log.d("RecadosCustomAdapter", "text: " + rInfo.getTextInfo());
+                                String read_date = rInfo.getDate();
+                                String read_text = rInfo.getTextInfo();
+
+                                String selected_date = recadosList.get(position).getDate();
+                                String selected_text = recadosList.get(position).getTextInfo();
+
+                                Log.d("RecadosCustomAdapter", "read_text: " + read_text);
+                                Log.d("RecadosCustomAdapter", "selected_text: " + selected_text);
+
+                                if(read_text != null & selected_text != null & read_date != null & selected_date !=null){
+                                    if(read_text.equals(selected_text) & read_date.equals(selected_date)){
+                                        buttonVisto.setEnabled(false);
+                                    }
+                                }
+
+                                //Log.d("RecadosCustomAdapter", "date: " + rInfo.getDate());
+                                //Log.d("RecadosCustomAdapter", "text: " + rInfo.getTextInfo());
                             }
                         }
                         @Override
