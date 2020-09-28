883b01373b5ead77156e3c8daf7db388fe4c7bca
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2020-09-28 19:07:11 -0300

    Refactor layout of conteudo screen

--- app/src/main/java/com/cristianerm/pd2/ConteudoActivity.java
+++ app/src/main/java/com/cristianerm/pd2/ConteudoActivity.java
@@ -1,6 +1,7 @@
 package com.cristianerm.pd2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,22 +10,27 @@ import android.widget.ImageButton;
 
 public class ConteudoActivity extends AppCompatActivity {
 
-    ImageButton voltar;
+    Toolbar toolbar_conteudo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conteudo);
 
-        voltar = (ImageButton) findViewById(R.id.buttonVoltarConteudo);
+        toolbar_conteudo = (Toolbar) findViewById(R.id.tool_bar_conteudo);
+        setSupportActionBar(toolbar_conteudo);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar_conteudo.setTitle("");
+        toolbar_conteudo.setSubtitle("");
 
-        voltar.setOnClickListener(new View.OnClickListener() {
+        toolbar_conteudo.setNavigationOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent i;
                 i = new Intent(ConteudoActivity.this, MenuActivity.class);
                 startActivity(i);
             }
         });
+
     }
 }
