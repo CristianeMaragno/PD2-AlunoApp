aa1fe0fe8391504c73f337e90e6eec6d53ceaaa7
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2020-10-02 21:33:01 -0300

    Separates texts that contain dates and texts that contain the message

--- app/src/main/java/com/cristianerm/pd2/ImageAdapterDiario.java
+++ app/src/main/java/com/cristianerm/pd2/ImageAdapterDiario.java
@@ -30,7 +30,8 @@ public class ImageAdapterDiario extends RecyclerView.Adapter<ImageAdapterDiario.
     @Override
     public void onBindViewHolder(ImageViewHolder holder, int position) {
         DiarioTurmaInformation uploadCurrent = mDiarioInformation.get(position);
-        holder.text_mensagem_diario.setText(uploadCurrent.getDate() + "\n \n" + uploadCurrent.getMensagem());
+        holder.text_date_diario.setText(uploadCurrent.getDate());
+        holder.text_mensagem_diario.setText(uploadCurrent.getMensagem());
 
         if(uploadCurrent.getImageUrl() == "No image"){
             holder.image_view_diario.setVisibility(View.GONE);
@@ -47,10 +48,12 @@ public class ImageAdapterDiario extends RecyclerView.Adapter<ImageAdapterDiario.
         return mDiarioInformation.size();
     }
     public class ImageViewHolder extends RecyclerView.ViewHolder {
+        public TextView text_date_diario;
         public TextView text_mensagem_diario;
         public ImageView image_view_diario;
         public ImageViewHolder(View itemView) {
             super(itemView);
+            text_date_diario = itemView.findViewById(R.id.text_diario_date);
             text_mensagem_diario = itemView.findViewById(R.id.text_mensagem_diario);
             image_view_diario = itemView.findViewById(R.id.image_view_diario);
         }
