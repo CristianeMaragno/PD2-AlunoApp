a3ec10a7768b782b3f2405eb5015672deb4c3318
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2020-10-05 18:44:15 -0300

    Solve bug on diario screen. When there is no image, makes item smaller

--- app/src/main/java/com/cristianerm/pd2/ImageAdapterDiario.java
+++ app/src/main/java/com/cristianerm/pd2/ImageAdapterDiario.java
@@ -2,10 +2,12 @@ package com.cristianerm.pd2;
 
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +15,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+
+import static android.view.View.GONE;
 
 public class ImageAdapterDiario extends RecyclerView.Adapter<ImageAdapterDiario.ImageViewHolder> {
     private Context mContext;
@@ -33,9 +37,8 @@ public class ImageAdapterDiario extends RecyclerView.Adapter<ImageAdapterDiario.
         holder.text_date_diario.setText(uploadCurrent.getDate());
         holder.text_mensagem_diario.setText(uploadCurrent.getMensagem());
 
-        if(uploadCurrent.getImageUrl() == "No image"){
+        if(uploadCurrent.getImageUrl().equals("No image")){
             holder.image_view_diario.setVisibility(View.GONE);
-            //error, espace of the imageview is still there
         }else{
             Picasso.get()
                     .load(uploadCurrent.getImageUrl())
@@ -51,11 +54,14 @@ public class ImageAdapterDiario extends RecyclerView.Adapter<ImageAdapterDiario.
         public TextView text_date_diario;
         public TextView text_mensagem_diario;
         public ImageView image_view_diario;
+        public LinearLayout hide_layout;
+
         public ImageViewHolder(View itemView) {
             super(itemView);
             text_date_diario = itemView.findViewById(R.id.text_diario_date);
             text_mensagem_diario = itemView.findViewById(R.id.text_mensagem_diario);
             image_view_diario = itemView.findViewById(R.id.image_view_diario);
+            hide_layout = itemView.findViewById(R.id.layout_to_hide);
         }
     }
 }
